3667193be6d220e75e4cbc042ac269159bb50bca
anhtudo97 <anhtudo97@gmail.com>
2021-07-04 21:53:54 +0700

    feat: top section

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+import { Navbar } from '../../components/NavBar';
+import { TopSection } from './topSection';
 
 const PageContainer = styled.div`
   ${tw`flex flex-col items-center w-full h-full overflow-x-hidden `}
 `;
 
-export const HomePage = (): React.ReactElement => {
-  return <PageContainer>helloworld</PageContainer>;
+export const HomePage = () => {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+    </PageContainer>
+  );
 };
