6de8ffdf6994fb1e2c99e02fcd0674ae56674615
anhtudo97 <anhtudo97@gmail.com>
2021-07-10 00:09:39 +0700

    feat: config type

--- src/app/graphql.ts
+++ src/app/graphql.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   uri: `${process.env.REACT_APP_API_URL}/graphql` as any,
